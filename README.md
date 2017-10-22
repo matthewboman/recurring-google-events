0e820ed610d35abe3a8ac97de5080cd03e572a35
Crash Springfield <ccrsh@riseup.net>
2017-10-22 16:04:04 -0400

    update algorithms and README

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # recurring-google-events
 A function for getting events from Google calendars and formatting them for react-big-calendar.
 
+**This repository is no longer maintained. Checkout (react-google-calendar)[https://github.com/crashspringfield/react-google-calendar] for a maintained implementation**
+
 ## About
 This gets events from one or many Google calendars. It returns single events and reoccurring events. It currently returns the following types of recoccurring events:
 
-  * weekly events (e.g. every Monday)
-  * monthly events by date (e.g. [first of the month](https://www.youtube.com/watch?v=PArF9k2SbQk))
-  * monthy events by day (e.g. first Friday).
 
-The function takes two arguments `getAllCalendars(weeklyRecurrence, monthlyRecurrence)` where you can specify how many itterations of the weekly and monthly events it returns.
-
-## Contributing
-This is a work in progress. There are probably edge cases and types of reoccurring events it doesn't handle yet. Raise an issue to add a feature or make a correction
+* single events
+* daily events (e.g. every day, every n days)
+* weekly events (e.g. every Monday)
+* monthly events by date (e.g. [first of the month](https://www.youtube.com/watch?v=PArF9k2SbQk))
+* monthy events by day (e.g. first Friday)
 
 ## License
 MIT
--- index.js
+++ index.js
@@ -2,148 +2,28 @@ import axios from 'axios'
 import * as Promise from 'bluebird'
 import moment from 'moment'
 
-const API_KEY = '' // Google calendar API key
+import { handleDaily, handleDateOfMonth, handleDayOfMonth, handleWeekly } from './algorithms'
+
+const GOOGLE_API_KEY = ''
 const calendars = [
-  {
-    name: 'example',
-    url: 'example@gmail.com'
-  },
+  { name: '', demo: '' },
 ]
-
-/*
- * events that occure the same day of every week
- * (i.e. every Monday)
-*/
-const handleWeekly = (calendar, recurrence, e) => {
-  const start = moment(e.start.dateTime)
-  const end = moment(e.end.dateTime)
-  let reoccurringEvents = []
-  let add = 1
-
-  while (recurrence > 0) {
-    const reoccurringEvent = {
-      eventType: calendar.name,
-      creator: e.creator,
-      end: end.clone().add(add, 'week')._d,
-      gLink: e.htmlLink,
-      description: e.description,
-      location: e.location,
-      start: start.clone().add(add, 'week')._d,
-      title: e.summary,
-      meta: e
-    }
-    reoccurringEvents.push(reoccurringEvent)
-    recurrence --
-    add ++
-  }
-  return reoccurringEvents
-}
-
-/*
- * events that occur the same date of every month
- * (i.e. the 1st, the 8th)
-*/
-const handleDateOfMonth = (calendar, recurrence, e) => {
-  const start = moment(e.start.dateTime)
-  const end = moment(e.end.dateTime)
-  let reoccurringEvents = []
-  let add = 1
-
-  while (recurrence > 0) {
-    const reoccurringEvent = {
-      eventType: calendar.name,
-      creator: e.creator,
-      end: end.clone().add(add, 'months')._d,
-      gLink: e.htmlLink,
-      description: e.description,
-      location: e.location,
-      start: start.clone().add(add, 'months')._d,
-      title: e.summary,
-      meta: e
-    }
-    reoccurringEvents.push(reoccurringEvent)
-    recurrence --
-    add ++
-  }
-  return reoccurringEvents
-}
-
-/*
- * events that occur the same day of the month
- * (i.e. first Friday, last Monday)
- */
-const handleDayOfMonth = (calendar, recurrence, e) => {
-  const start = moment(e.start.dateTime)
-  const end = moment(e.end.dateTime)
-  const day = start.day()
-  const date = start.date()
-  let counter
-  if (date <= 7) {
-    counter = 1
-  } else if ((date >7) && (date <= 14)) {
-    counter = 7
-  } else if ((date > 14) && (date <= 21)) {
-    counter = 14
-  } else if ((date > 21) && (date <= 28)) {
-    counter = 21
-  } else {
-    counter = 28
-  }
-
-  let reoccurringEvents = []
-
-  while (recurrence > 0) {
-    let tempCounter = counter
-    // doesn't work with Moment
-    let nextStart = new Date(start.year(), start.month() + recurrence,
-                              tempCounter, start.hour(), start.minutes())
-    let nextEnd = new Date(end.year(), end.month() + recurrence,
-                            tempCounter, end.hour(), end.minutes())
-
-    while (tempCounter < 31) {
-      let isEqual = nextStart.getDay() == start.day()
-
-      if (isEqual) {
-        const reoccurringEvent = {
-          eventType: calendar.name,
-          creator: e.creator,
-          end: nextEnd,
-          gLink: e.htmlLink,
-          description: e.description,
-          location: e.location,
-          start: nextStart,
-          title: e.summary,
-          meta: e
-        }
-        reoccurringEvents.push(reoccurringEvent)
-        tempCounter = counter
-        break
-      }
-      nextStart = new Date(start.year(), start.month() + recurrence,
-        tempCounter, start.hour(), start.minutes())
-      nextEnd = new Date(end.year(), end.month() + recurrence,
-        tempCounter, end.hour(), end.minutes())
-        
-      tempCounter ++
-    }
-    recurrence --
-  }
-  return reoccurringEvents
-}
 
 export default {
 
-  getAllCalendars: (weeklyRecurrence, monthlyRecurrence) => Promise.map(calendars, (calendar) => {
-    const url = `https://content.googleapis.com/calendar/v3/calendars/${calendar.url}/events?key=${API_KEY}`
-    return axios.get(url)
+  getAllCalendars: (GOOGLE_API_KEY, calendars, dailyRecurrence, weeklyRecurrence, monthlyRecurrence) => Promise.map(calendars, (calendar) => {
+    return axios.get(`https://content.googleapis.com/calendar/v3/calendars/${calendar.url}/events?key=${GOOGLE_API_KEY}`)
       .then(res => {
         const items = res.data.items.filter(item => item.status != "cancelled")
         const oneTime = items.filter(item => !item.recurrence).map(e => {
+          // account for all day events
+          const start = e.start.date ? new Date(`${e.start.date}T08:00:00`) : new Date(e.start.dateTime)
+          const end = e.end.date ? new Date(`${e.start.date}T05:00:00`) : new Date(e.end.dateTime)
           return {
             title: e.summary,
             eventType: calendar.name,
-            start: new Date(e.start.dateTime).toString(),
-            end: new Date(e.end.dateTime).toString(),
+            start: start,
+            end: end,
             description: e.description,
             location: e.location,
             glink: e.htmlLink,
@@ -155,6 +35,11 @@ export default {
             const r = event.recurrence[0].split(';')
             return { e: event, r }
           })
+
+        const daily = reoccurring.filter(item => item.r[0] == "RRULE:FREQ=DAILY")
+          .map(item => item.e)
+        const reoccurringDaily = [].concat.apply([], daily
+          .map(e => handleDaily(calendar, dailyRecurrence, e)))
 
         const weekly = reoccurring.filter(item => item.r[0] == "RRULE:FREQ=WEEKLY")
           .map(item => item.e)
@@ -174,12 +59,14 @@ export default {
 
         const allEvents = [].concat(
           oneTime,
+          reoccurringDaily,
           reoccurringWeekly,
           reoccurringDateOfMonth,
           reoccurringDayOfMonth
         )
         return allEvents
       })
-  }).then(allEvents => [].concat.apply([], allEvents))
+  })
+  .then(allEvents => [].concat.apply([], allEvents))
 
 }
